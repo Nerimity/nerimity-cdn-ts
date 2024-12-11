2fece94bdb2e23ae9d95a67c59e5cfd3df41ac94
Ripley White <anekodot@gmail.com>
2024-12-11 21:24:53 +0000

    Use PixieDust to compress images to WebP

--- src/middlewares/compressImage.middleware.ts
+++ src/middlewares/compressImage.middleware.ts
@@ -1,20 +1,53 @@
 import { Request, Response } from "hyper-express";
-import {
-  compressImage,
-  CompressImageOptions,
-  pointsToDimensions,
-  removeFile,
-} from "../utils/imageMagick";
 import path from "path";
 import { tempDirPath } from "../utils/Folders";
+import fs from "fs";
+import { exec } from "node:child_process";
+import { promisify } from "util";
+import { imageSize } from 'image-size'
+import { removeFile } from "../utils/imageMagick";
 
-type Opts = Omit<
-  Omit<Omit<Omit<CompressImageOptions, "tempPath">, "filename">, "newPath">,
-  "crop"
-> & {
-  allowCrop?: boolean;
-};
-export const compressImageMiddleware = (opts: Opts) => {
+const execPromise = promisify(exec);
+
+interface CompressImageOptions {
+  tempPath: string;
+  newPath: string;
+  filename: string;
+}
+
+interface CompressedImage {
+  filesize: number;
+  filename: string;
+  newFilename: string;
+  height: number;
+  width: number;
+  animated: boolean;
+}
+
+async function compressImage(opts: CompressImageOptions) {
+  const newPath = opts.newPath + "/" + path.parse(opts.filename).name + ".webp";
+
+  await execPromise(`./pixiedust ${opts.tempPath} ${newPath}`).then(async (err) => {
+    if (err.stderr != "") {
+      console.log(`Failed to compress!! ${err.stderr}`);
+      return [null, "Something went wrong while compressing image. Error: " + err] as const;
+    }
+  });
+
+  const dimensions = imageSize(newPath);
+
+  return [
+    {
+      path: newPath,
+      filename: path.parse(newPath).name,
+      height: dimensions.height,
+      width: dimensions.width,
+      filesize: await fs.promises.stat(newPath).then((stat) => stat.size),
+      animated: (path.parse(opts.filename).ext == ".gif")
+    }, null] as const;
+}
+
+export const compressImageMiddleware = () => {
   return async (req: Request, res: Response) => {
     if (req?.file?.shouldCompress) {
       let closed = false;
@@ -23,27 +56,10 @@ export const compressImageMiddleware = (opts: Opts) => {
       });
       const tempFilePath = path.join(tempDirPath, req.file.tempFilename);
 
-      let strPoints = req.query.points as string | undefined;
-      let crop:
-        | [number, number, number, number]
-        | [number, number]
-        | undefined = undefined;
-      if (opts.allowCrop) {
-        const [dimensions, points, dimErr] = pointsToDimensions(strPoints);
-        if (dimErr) {
-          return res.status(403).json(dimErr);
-        }
-        crop = dimensions
-          ? [dimensions.width, dimensions.height, points[0]!, points[1]!]
-          : [opts.size[0], opts.size[1]];
-      }
-
       const [result, err] = await compressImage({
-        ...opts,
         tempPath: tempFilePath,
         newPath: tempDirPath,
-        filename: req.file.tempFilename,
-        crop,
+        filename: req.file.tempFilename
       });
 
       if (err) {
@@ -52,31 +68,26 @@ export const compressImageMiddleware = (opts: Opts) => {
         });
         return;
       }
+
       if (closed) {
         removeFile(result.path);
         removeFile(tempFilePath);
 
         return;
       }
+
       req.file.filesize = result.filesize;
-      req.file.compressedFilename = result.newFilename;
-      req.file.compressedHeight = result.dimensions.height;
-      req.file.compressedWidth = result.dimensions.width;
-      req.file.animated = !!result.gif;
+      req.file.compressedFilename = result.filename;
+      req.file.compressedHeight = result.height;
+      req.file.compressedWidth = result.width;
+      req.file.animated = result.animated;
       req.file.originalFilename =
-        path.parse(req.file.originalFilename).name +
-        path.parse(result.newFilename).ext;
-      if (result.gif) {
-        req.file.mimetype = "image/gif";
-      } else {
-        req.file.mimetype = "image/webp";
-      }
-      if (req.file.tempFilename !== req.file.compressedFilename) {
-        removeFile(tempFilePath);
-        req.file.tempFilename =
-          path.parse(req.file.tempFilename).name +
-          path.parse(result.newFilename).ext;
-      }
+        path.parse(req.file.originalFilename).name + ".webp";
+      req.file.tempFilename =
+          path.parse(req.file.tempFilename).name + ".webp";
+      req.file.mimetype = "image/webp";
+
+      removeFile(tempFilePath);
     }
   };
 };
